ad0ea140b283e430b85f00d874648e58c58cac29
Nariman <islamovnariman@gmail.com>
2020-01-18 22:15:53 +0300

    Задачка 18.01.2020, Friend or Foe?

--- script.js
+++ script.js
@@ -225,3 +225,18 @@ function XO(str) {
 }
 XO('xxoo');
 */
+
+// 18.01.2020
+// https://www.codewars.com/kata/55b42574ff091733d900002f/train/javascript
+/*
+function friend(friends){
+    var result = [];
+    for (var i = 0; i < friends.length; i++) {
+        if (friends[i].length === 4) {
+            result.push(friends[i]);
+        }
+    }
+    return result;
+}
+friend(["Ryan", "Kieran", "Mark"]);
+*/
